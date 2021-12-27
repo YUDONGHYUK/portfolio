c9e2bb6530700fd71c319afe757528c177fbc95e
YUDONGHYUK <91944649+YUDONGHYUK@users.noreply.github.com>
2021-12-27 16:31:22 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -8,3 +8,17 @@ document.addEventListener("scroll", () => {
     navbar.classList.add("navbar--dark");
   } else [navbar.classList.remove("navbar--dark")];
 });
+
+// Handle scrolling when tapping on the navbar menu
+
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
